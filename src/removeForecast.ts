8c1d4f250bfd061a993530f357d095e86eabcfc8
Garieldas <dainiusrainys99@gmail.com>
2025-05-21 22:38:14 +0300

    Edited remove function to remove forecast from local storage

--- src/removeForecast.ts
+++ src/removeForecast.ts
@@ -1,6 +1,11 @@
+import saveToStorage from "./saveToStorage";
+
 const removeForecast = (id: string) =>
 {
     const forecastCard = document.getElementById(id);
+    const storedForecasts = JSON.parse(localStorage.getItem('forecast')!);
+    const filtered = storedForecasts.filter((forecast: {id: string}) => forecast.id !== id);
+    saveToStorage(filtered);
     forecastCard?.remove();
 }
 
